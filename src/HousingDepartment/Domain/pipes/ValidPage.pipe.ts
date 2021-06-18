f99798703c9aa2c0bc67569599d5cba3d28f150a
Abelardo León González <abelardolg@gmail.com>
2021-06-18 12:20:58 +0200

    Fix code.

--- src/HousingDepartment/Domain/pipes/ValidPage.pipe.ts
+++ src/HousingDepartment/Domain/pipes/ValidPage.pipe.ts
@@ -3,6 +3,8 @@ import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 @Injectable()
 export class ValidPage implements PipeTransform<number, number> {
   transform(value: any, metadata: ArgumentMetadata) {
+
     if (value < 0) return 0;
+    return value;
   }
 }
