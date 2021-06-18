505cf3d832a0c05be70fbb1e2ad808e0453e2168
Abelardo León González <abelardolg@gmail.com>
2021-06-18 13:00:20 +0200

    Refactored code (removing guard).

--- src/HousingDepartment/Infrastructure/getAdsHousing.controller.ts
+++ src/HousingDepartment/Infrastructure/getAdsHousing.controller.ts
@@ -1,23 +1,21 @@
-import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, ParseIntPipe, Res, UseGuards } from '@nestjs/common';
 
 import { GetAdsHousingUseCase } from '../Domain/useCases/getAdsHousing.usecase';
 
 import { ValidPage } from '../Domain/pipes/ValidPage.pipe';
-import { ValidationGuard } from '../Application/guards/validation.guard';
 
 @Controller()
-@UseGuards(ValidationGuard)
 export class GetAdsHousingController {
   constructor(private readonly getAdsHousingUseCase: GetAdsHousingUseCase){}
+
+  @Get("getAllAdsHousing")
+  async getAllAdsHousing() {
+    return await this.getAdsHousingUseCase.getAllAdsHousing();
+  }
 
   @Get(':fromPage')
   async getAdsHousing(@Param('fromPage', ValidPage) fromPage: number) {
     return await this.getAdsHousingUseCase.getAdsHousing(fromPage);
-  }
-
-  @Get()
-  async getAllAdsHousing() {
-    return await this.getAdsHousingUseCase.getAllAdsHousing();
   }
 }
 
