abf8e4462e2284f1de808002c9a4404a5ab64995
Abelardo León González <abelardolg@gmail.com>
2021-06-18 11:37:36 +0200

    Added ValidPage pipe

--- src/HousingDepartment/Infrastructure/getAdsHousing.controller.ts
+++ src/HousingDepartment/Infrastructure/getAdsHousing.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 
 import { GetAdsHousingUseCase } from '../Domain/useCases/getAdsHousing.usecase';
 
+import { ValidPage } from '../Domain/pipes/ValidPage.pipe';
+
 @Controller()
 export class GetAdsHousingController {
   constructor(private readonly getAdsHousingUseCase: GetAdsHousingUseCase){}
 
   @Get(':fromPage')
-  async getAdsHousing(@Param('fromPage', ParseIntPipe) fromPage: number) {
+  async getAdsHousing(@Param('fromPage', ValidPage) fromPage: number) {
     return await this.getAdsHousingUseCase.getAdsHousing(fromPage);
   }
 
